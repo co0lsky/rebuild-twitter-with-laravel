a4ae01855d84f9b1c390c2ce4893620abd787ef9
Sky Chin <sky@iteachyouhowtocode.com>
2017-04-02 13:55:16 +0800

    Ajax

--- app/Tweet.php
+++ app/Tweet.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
@@ -10,4 +11,7 @@ class Tweet extends Model
         'user_id', 'body',
     ];
 
+    public function getCreatedAtAttribute($value) {
+        return Carbon::createFromFormat('Y-m-d h:i:s', $value)->diffForHumans();
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,20 +5,58 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Timeline</div>
 
-                <div class="panel-body">
-                    @forelse ($user->timeline() as $tweet)
-                        <a href="#" class="list-group-item">
-                            <h4 class="list-group-item-heading">{{ $tweet->body }}</h4>
-                            <p class="list-group-item-text">{{ $tweet->created_at }}</p>
-                        </a>
-                    @empty
-                        <p>No tweet</p>
-                    @endforelse
+
+                <div class="panel-body" id="list-1" v-infinite-scroll="loadMore" infinite-scroll-disabled="busy" infinite-scroll-distance="10">
+                    <a href="#" class="list-group-item" v-for="tweet in items">
+                        <h4 class="list-group-item-heading">@{{ tweet.body }}</h4>
+                        <p class="list-group-item-text">@{{ tweet.created_at }}</p>
+                    </a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+@endsection
+
+@section('script')
+<script src="https://unpkg.com/vue-resource@1.2.0/dist/vue-resource.min.js"></script>
+<script src="https://unpkg.com/vue-infinite-scroll@2.0.0"></script>
+<script type="text/javascript">
+    var page = 1;
+
+    new Vue({
+      el: '#list-1',
+      data: {
+        page: 1,
+        items: [],
+        busy: false
+      },
+      methods: {
+        loadMore: function() {
+            this.busy = true;
+
+            var url = '/timeline' + (this.page > 1 ? '?page=' + this.page : '');
+
+            this.$http.get(url)
+            .then(response => {
+                var data = response.body;
+
+                // Push the response data into items
+                for (var i = 0, j = data.length; i < j; i++) {
+                  this.items.push(data[i]);
+                }
+
+                // If the response data is empty,
+                // disable the infinite-scroll
+                this.busy = (j < 1);
+
+                // Increase the page number
+                this.page++;
+            });
+        }
+      }
+    });
+</script>
 @endsection
